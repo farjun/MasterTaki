ce3aa960b304a1210a28d841f9c15da2cc42ad13
omer ben haim <omerbh1@gmail.com>
2018-07-04 18:40:07 +0300

    init

--- Agents/DeterministicAgents/ReflexAgentInterface.py
+++ Agents/DeterministicAgents/ReflexAgentInterface.py
@@ -4,4 +4,7 @@ class HeuristicReflexAgent(PlayerInterface):
     def __init__(self, game):
         super().__init__("HeuristicReflexAgent", game)
 
+    def choose_action(self,all_leagal_actions):
+
+
 
