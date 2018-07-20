269331cc749b965330162e9ed45d0d724ab6a3db
omer bh <omerbh1@gmail.com>
2018-07-20 13:26:04 +0300

    pomdp init

--- Agents/DeterministicAgents/MDPAgent.py
+++ Agents/DeterministicAgents/MDPAgent.py
@@ -6,10 +6,12 @@ import numpy as np
 class MDPAgent(PlayerInterface):
     def __init__(self, game):
         super().__init__("POMDPAgent", game)
+        self.is_fully_observable = True
 
     def choose_action(self):
         """
         :return: best action every
         """
-        cur_state = self.game.get_cur_state(self.cards, full_state=True)
+        cur_state = self.game.get_cur_state(self.cards, full_state = self.is_fully_observable)
+
 
